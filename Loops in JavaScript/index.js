ce337afff5b6b1ee92c0cd6e4a6037b73275127d
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-04-10 18:50:20 +0530

    Learning about loop in JavaScript

---
+++ Loops in JavaScript/index.js
@@ -0,0 +1,40 @@
+// console.log("Hello Adity")
+// let a=5;
+//     // initial value of loop ; loop condition; increment
+
+
+//     // Simple for loop is used to loop a block of code number of times
+// for (let i=1;i<11;i++){
+//     console.log(a,"X",i,'=',a*i)
+// }
+// let object={
+//     "name":"Nishu",
+//     "title":"singh",
+//     "company":"Tcs"
+// }
+
+
+// // used to loop in the object
+// for (const key in object) {
+//     const value=object[key]
+//         console.log(key,value)  
+// }
+
+// // used to loop in string
+// for (const c of 'object') {
+//     console.log(c)
+// }
+
+// let i=2;
+// while (i<5) {
+//     console.log(i)
+//     i++;
+    
+// }
+
+// Do while loop it is basially used to run the block of code only one time. if its conditions is false despite that it will execute the code once
+let b=8;
+do{
+    console.log(b)
+    b++
+}while(b<7)
