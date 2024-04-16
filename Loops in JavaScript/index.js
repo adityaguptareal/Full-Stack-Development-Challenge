f9ef97b247618506f205e29fc7be5fce493b732e
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-04-16 23:20:20 +0530

    Learns Loops

--- Loops in JavaScript/index.js
+++ Loops in JavaScript/index.js
@@ -28,7 +28,7 @@
 // let i=2;
 // while (i<5) {
 //     console.log(i)
-//     i++;
+//     i++;34
     
 // }
 
@@ -38,3 +38,56 @@ do{
     console.log(b)
     b++
 }while(b<7)
+
+// let a=1;
+// for (let i = 0; i<100;i++){
+//     console.log(a+i)
+// }
+
+// let a=1;
+// for (let i = 0; i<100; i++){
+//     console.log(a+i);
+// }
+
+// let c=2
+// for (let i=1; i<11;i++){
+//     console.log(c +"X" + i +"="+c*i)
+// }
+
+// var d="Aditya Will Be Millionarie";
+
+let obj={
+    "name":"Aditya",
+    "role":"Progameer",
+"Salary":100000}
+
+// for (const key in obj){
+//         const element = obj[key];
+//         console.log(element)
+//     }
+
+
+// for (const first in obj){
+//     var a=obj[first]
+//     console.log(a)
+// }
+
+// for (const first in obj){
+//     console.log(first,obj[first])
+// }
+
+for (const c of "harry"){
+    console.log(c)
+}
+
+// let i=1;
+// while (i<8){
+//     console.log("aditya")
+//     i++
+// }
+// let i=0;
+// do {
+// console.log(i)
+// i++
+// }
+// while(i<6)
