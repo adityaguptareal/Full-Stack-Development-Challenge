8b81d6dae8bc44282b237f38019a812be6460817
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-05-02 18:39:53 +0530

    Adding Features

--- JavaScript Exercise 13/script.js
+++ JavaScript Exercise 13/script.js
@@ -1,3 +1,4 @@
+let btn=document.querySelector(".update")
 function createCard(title,cname,views,monthOld,duration,thumbnail){
     let view_str
     if(views<=1000000){
@@ -17,7 +18,7 @@ function createCard(title,cname,views,monthOld,duration,thumbnail){
     
     <div class="text">
         <h3 class="video_title">${title}</h3>
-        <div class="video_details">${cname}. ${view_str}.${monthOld}</div>
+        <div class="video_details"><span id="channel_name">${cname}</span>. ${view_str}.${monthOld}</div>
     </div>
 </div>`
 document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + card;
@@ -26,4 +27,10 @@ document.querySelector(".container").innerHTML = document.querySelector(".contai
 
 // Calling function
 
-createCard("Lorem, ipsum dolor sit amet consectetur ","Aditya Kumar",30000,"9 Months ago","32:45","https://i.ytimg.com/vi/BGeDBfCIqas/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBuhpUj-ErJicHq24jgB_3K2Chl_Q")
+
+
+
+btn.addEventListener("click",function(){
+    createCard("Sigma Web Development adity Kumar Gupta Tesitng heading title Course - Tutorial #5 ","Aditya Kumar",30000,"9 Months ago","32:45","https://i.ytimg.com/vi/BGeDBfCIqas/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBuhpUj-ErJicHq24jgB_3K2Chl_Q")
+})
+
