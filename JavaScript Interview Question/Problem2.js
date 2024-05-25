50d0636b8579ea9598dd7c88ba0046c8cb049ce8
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-05-25 23:30:51 +0530

    Resolve merge conflicts and integrate remote changes

---
+++ JavaScript Interview Question/Problem2.js
@@ -0,0 +1,23 @@
+// The Double Trouble: You are tasked with writing a function that doubles each element in an array. However, there's a catch: if the array contains consecutive duplicate elements, only double one of them.
+
+let arr=["a","b","c","d","e","f","f","e","g","h"]
+
+function double_trouble(arr){
+    for (const element of arr) {    
+        if (element==element1) {
+        let double=arr
+        } else {
+            double=arr.concat(arr)
+            console.log(double);
+        }    
+    }
+    
+}
+
+
+
+
+
+// function double_trouble(arr) {
+    
+// }
---
+++ JavaScript Interview Question/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+const myArray = [1, 2, 3, 4, 5];
+const doubledArray = myArray.map(item => item * 2);
+
+console.log("Original array:", myArray);
+console.log("Doubled array:", doubledArray);
