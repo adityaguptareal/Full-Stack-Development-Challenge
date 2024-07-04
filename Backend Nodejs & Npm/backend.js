7a8546e22fc36c480c434ab6c61f76da016387bb
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-07-04 14:11:36 +0530

    Push on github

---
+++ Backend Nodejs & Npm/backend.js
@@ -0,0 +1,4 @@
+console.log('Hey Hello world');
+
+// Steps to run the node js file
+// => type "node" and file name (you can use first letter and hit the tab button to autocomplete the name) in the termnial and hit enter
