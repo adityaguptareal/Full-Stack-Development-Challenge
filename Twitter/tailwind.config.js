e2151a4a8d43e0360156d4aac407d7d96b03eda9
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-07-30 23:00:21 +0530

    Uploading twitter Project

---
+++ Twitter/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        archivo: ["Archivo", "sans-serif"],
+      },
+      colors: {
+        grey: "#1b1b1b",
+      },
+    },
+  },
+  plugins: [],
+}
