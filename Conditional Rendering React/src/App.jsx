cc8556c95d751ede66ae9e0ff18f312b703a0253
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-08-08 13:13:49 +0530

    fix

--- Conditional Rendering React/src/App.jsx
+++ Conditional Rendering React/src/App.jsx
@@ -6,10 +6,25 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [showbtn, setshowbtn] = useState(false)
-  const Todo=()=>{
-    return(
+  const [todos, setTodos] = useState([
+    {
+      "title": "Complet Online Class",
+      "desc": "You have to complete the online class",
+    },
+    {
+      "title": "Water",
+      "desc": "You have to drink water",
+    },
+    {
+      "title": "Web Development",
+      "desc": "Complete a web development Project",
+    },
+  ])
+  const Todo = ({todo}) => {
+    return (
       <>
-      <div className="todo">Hi this is Aditya</div>
+        <div className="todo">{todo.title}</div>
+        <div className="todo">{todo.desc}</div>
       </>
     )
   }
@@ -27,15 +42,19 @@ function App() {
       <h1>Vite + React</h1>
 
       {/* Conditional Rendering When we don't want to display anything  */}
-        {/* {showbtn && <button>Set Btn True</button>
+      {/* {showbtn && <button>Set Btn True</button>
         } */}
-        {showbtn ? <button>Set Btn True</button> :<button>Set Btn False</button> 
-        }
+      {showbtn ? <button>Set Btn True</button> : <button>Set Btn False</button>
+      }
+      {/* Displaying  the list using map */}
+       {todos.map(todo=>{
+          return <Todo todo={todo} />
+        })}
       <div className="card">
         <button onClick={() => setshowbtn(!showbtn)}>
           count is {count}
         </button>
-      <Todo/>
+       
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
